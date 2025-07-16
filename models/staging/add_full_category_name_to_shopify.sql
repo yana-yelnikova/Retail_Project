f98bb91b89f6c9d1d397d84485583ca8e595e912
Yana Yelnikova <yana.yelnikova@gmail.com>
2025-07-16 20:14:53 +0000

    commit

---
+++ models/staging/add_full_category_name_to_shopify.sql
@@ -0,0 +1,23 @@
+select
+    *,
+    case
+        category
+        when 'SPRT'
+        then 'Sports'
+        when 'HOME'
+        then 'Home Goods'
+        when 'BEAU'
+        then 'Beauty'
+        when 'AUTO'
+        then 'Automotive'
+        when 'FOOD'
+        then 'Food'
+        when 'TOYS'
+        then 'Toys'
+        when 'ELEC'
+        then 'Electronics'
+        when 'FASH'
+        then 'Fashion'
+        else category
+    end as full_category_name
+from {{ source("retail_data", "SHOPIFY_ORDERS") }}  -- Ссылка на вашу исходную таблицу Shopify
